ce5b8c3f5015c4c5aacb7a1d2f2719ab63936b54
Corey Davis <coreyd303@gmail.com>
2014-06-23 11:09:15 -0600

    complete data_handler.rb w/test file

--- lib/data_handler.rb
+++ lib/data_handler.rb
@@ -1,42 +1,33 @@
 require 'csv'
 
-def clean_zipcode(zipcode)
-  if zipcode.nil?
-    "00000"
-  elsif zipcode.length < 5
-    zipcode.rjust(5,"0")
-  elsif zipcode.length > 5
-    zipcode[0..4]
-  else
-    zipcode
+class DataHandler
+  def clean_zipcode(zipcode)
+    if zipcode.nil? || zipcode.length > 5
+      "00000"
+    elsif zipcode.length < 5
+      zipcode.rjust(5, "0")
+    else
+      zipcode
+    end
   end
-end
 
-def clean_first_name(first_Name)
-  first_Name.downcase.capitalize
-end
+  def clean_first_name(first_name)
+    first_name.downcase
+  end
 
-def clean_last_name(last_Name)
-  last_Name.downcase.capitalize
-end
+  def clean_last_name(last_Name)
+    last_Name.downcase
+  end
 
-# def address_builder(street, city, state)
-#   puts "#{street}, #{city}, #{state}"
-# end
+  def clean_phone_number(phone_number)
+    phone_number.scan(/\d/).join
+  end
 
-def clean_phone_number(HomePhone)
+  def clean_city(city)
+    city.downcase
+  end
 
-end
-
-puts "EventManager initialized."
-
-contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-
-contents.each do |row|
-  first_name = clean_first_name(row[:first_name])
-  last_name = clean_last_name(row[:last_name])
-  address = address_builder(row[:address])
-  zipcode = clean_zipcode(row[:zipcode])
-
-  puts "#{last_name}, #{first_name}, #{address}, #{zipcode}"
+  def clean_state(state)
+    state.upcase
+  end
 end
--- test/data_handler_test.rb
+++ test/data_handler_test.rb
@@ -1,11 +1,85 @@
 gem 'minitest', '~>5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/data_handler'
+require './lib/data_handler'
 
-class Data_Handler_Test < Minitest::Test
+class DataHandlerTest < Minitest::Test
+  attr_reader :data_handler
 
-  def test_it_can_load_a_new_file_with_a_clean_a_zipcode
-    Data_Handler.new
+  def setup
+    @data_handler = DataHandler.new
   end
+
+  def test_it_exists
+    assert DataHandler
+  end
+
+  def test_it_does_nothing_to_a_good_zipcode
+    assert_equal "80202", data_handler.clean_zipcode("80202")
+  end
+
+  def test_a_nil_zipcode_returns_zeroes
+    assert_equal "00000", data_handler.clean_zipcode(nil)
+  end
+
+  def test_a_short_zipcode_gets_padded_with_zeroes
+    assert_equal "00123", data_handler.clean_zipcode("123")
+  end
+
+  def test_a_long_zipcode_returns_zeroes
+    assert_equal "00000", data_handler.clean_zipcode("123456")
+  end
+
+  def test_a_simple_first_name_is_lowercased
+    assert_equal "jeff", data_handler.clean_first_name("Jeff")
+  end
+
+  def test_a_mixed_case_first_name_is_lowered
+    assert_equal "jeff", data_handler.clean_first_name("JeFF")
+  end
+
+  def test_a_multiname_first_name_is_lowered
+    assert_equal "john paul", data_handler.clean_first_name("John Paul")
+  end
+
+  def test_a_multiname_last_name_is_lowered
+    assert_equal "raven hanssen", data_handler.clean_last_name("Raven Hanssen")
+  end
+
+  def test_a_good_phone_number_is_not_changed
+    assert_equal "5712223333", data_handler.clean_phone_number("5712223333")
+  end
+
+  def test_hyphens_are_removed_from_a_phone_number
+    assert_equal "1234567890", data_handler.clean_phone_number("123-456-7890")
+  end
+
+  def test_periods_are_removed_from_a_phone_number
+    assert_equal "1234567890", data_handler.clean_phone_number("123.456.7890")
+  end
+
+  def test_non_digits_are_removed_from_a_phone_number
+    assert_equal "1234567890", data_handler.clean_phone_number("(123) 456-7890")
+  end
+
+  def test_a_good_city_is_not_changed
+    assert_equal "denver", data_handler.clean_city("denver")
+  end
+
+  def test_a_mixed_case_city_is_lowered
+    assert_equal "pheonix", data_handler.clean_city("PheoNIx")
+  end
+
+  def test_a_multi_word_city_name_is_lowered
+    assert_equal "colorado springs", data_handler.clean_city("Colorado Springs")
+  end
+
+  def test_a_down_case_state_is_up_cased
+    assert_equal "AZ", data_handler.clean_state("az")
+  end
+
+  def test_a_mixed_case_state_is_up_cased
+    assert_equal "CO", data_handler.clean_state("Co")
+  end
+
 end
